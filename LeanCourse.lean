fd9b648b20011aac49089f6d87caa78439015a8c
Roman Soletskyi <romansoletskyi@gmail.com>
2024-09-21 00:48:53 +0200

    bookkeeping with doc-gen4

--- LeanCourse.lean
+++ LeanCourse.lean
@@ -1,2 +1,4 @@
 -- This module serves as the root of the `LeanCourse` library.
 -- Import modules here that should be built as part of the library.
+import LeanCourse.Basics.Calc
+import LeanCourse.Basics.Codestyle
--- lakefile.lean
+++ lakefile.lean
@@ -11,6 +11,9 @@ package «lean_course» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib «LeanCourse» where
   -- add any library configuration options here
